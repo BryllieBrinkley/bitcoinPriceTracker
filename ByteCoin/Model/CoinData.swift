6acd7b037604110126d70b74a09a29f7ae654498
JIbryll Brinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2024-02-21 16:56:49 -0500

    Add files via upload
    added files/ updated project

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Jibryll Brinkley on 2/21/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
